788bc1235268720a49dfc36018fc8ce96af9bbcb
nicolas cinzer <nicolas.cinzer00@gmail.com>
2021-12-07 16:54:25 -0300

    bind popup to markers

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 18,
 }).addTo(map);
 
-let description = document.getElementById("description")
-let address = document.getElementById("address")
-let phone = document.getElementById("phone")
-let category = document.getElementById("category")
-let coord = document.getElementById("coord")
+let description = document.getElementById("description");
+let address = document.getElementById("address");
+let phone = document.getElementById("phone");
+let category = document.getElementById("category");
+let coord = document.getElementById("coord");
 
-const addMarker = () => { L.marker(coord.value.split(",")).addTo(map)}
+const addMarker = () => {
+  L.marker(coord.value.split(","))
+    .bindPopup(
+      "<b>Descripción</b>: " +
+        description.value +
+        "</br><b>Dirección</b>: " +
+        address.value +
+        "</br><b>Teléfono</b>: " +
+        phone.value +
+        "</br><b>(X, Y)</b>: " +
+        coord.value.split(",") +
+        "</br><b>Categoría</b>: " +
+        category.value
+    )
+    .addTo(map);
+};
