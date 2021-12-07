6a51a4d8d8f02e4cec3a7c1860befa3bc16bdd9c
nicolas cinzer <nicolas.cinzer00@gmail.com>
2021-12-07 16:35:14 -0300

    add marker function

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "Nicolas Cinzer Aeroterra S.A. Test",
   maxZoom: 18,
 }).addTo(map);
+
+let description = document.getElementById("description")
+let address = document.getElementById("address")
+let phone = document.getElementById("phone")
+let category = document.getElementById("category")
+let coord = document.getElementById("coord")
+
+const addMarker = () => { L.marker(coord.value.split(",")).addTo(map)}
