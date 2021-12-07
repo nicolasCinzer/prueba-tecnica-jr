6cba371ebb7247ddbd2a7755fbba811de788f1fc
nicolas cinzer <nicolas.cinzer00@gmail.com>
2021-12-07 17:10:22 -0300

    Add clicking map giving coord info

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ const addMarker = () => {
     )
     .addTo(map);
 };
+
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on("click", onMapClick);
